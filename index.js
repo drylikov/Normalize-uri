12bf611dfc3fc3e05865f35d4d11b98d0dad491b
drylikov <denis.rylikov@protonmail.com>
2025-12-05 13:31:37 +0300

    Normalize-uri
    Legacy package to normalize a URI .

---
+++ index.js
@@ -0,0 +1,39 @@
+
+let implementation = identity
+
+try {
+  normalize('')
+  implementation = normalize
+  /* c8 ignore next */
+} catch {}
+
+/**
+ * Normalize `value`.
+ *
+ * @param {string} value URI
+ * @returns {string} Normalized URI
+ */
+export function normalizeUri(value) {
+  return implementation(value)
+}
+
+/**
+ * Normalize `value`.
+ *
+ * @param {string} value URI
+ * @returns {string} Normalized URI
+ */
+function normalize(value) {
+  return encodeURI(decodeURI(value))
+}
+
+/* c8 ignore next 9 */
+/**
+ * Normalize `value`.
+ *
+ * @param {string} value URI
+ * @returns {string} Normalized URI
+ */
+function identity(value) {
+  return value
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+# normalize-uri
+
+**Stability: Legacy**.
+This package is no longer recommended for use.
+It’s still covered by semantic-versioning guarantees and not yet deprecated,
+but use of this package should be avoided.
+Please use [`micromark-util-sanitize-uri`][micromark-util-sanitize-uri], which
+is a much better way to solve the same problems.
+
+Legacy [documentation for this
+package](https://github.com/drylikov/normalize_uri/tree/eb1d0d6)
+is still available in Git.
+
+<!-- Definitions -->
+
+[micromark-util-sanitize-uri]: https://github.com/micromark/micromark/tree/main/packages/micromark-util-sanitize-uri
